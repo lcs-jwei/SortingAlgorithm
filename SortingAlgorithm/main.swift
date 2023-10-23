f61b7a15cbe714ff14273e88fdd4f7b0ac4be174
Justin Wei <jwei@lcs.on.ca>
2023-10-23 08:56:22 -0400

    Added the data set, shown at what index has what values, learned to do -1 to not go out of range of the array.

--- SortingAlgorithm/main.swift
+++ SortingAlgorithm/main.swift
@@ -6,6 +6,24 @@
 //
 
 import Foundation
+// Create an empty array
+var dataSet: [Int] = []
 
-print("Hello, World!")
+// Populate the array to exactly match example given
+dataSet.append(6)
+dataSet.append(5)
+dataSet.append(3)
+dataSet.append(1)
+dataSet.append(8)
+dataSet.append(7)
+dataSet.append(2)
+dataSet.append(4)
+
+// Print the contents of the array
+print(dataSet)
+print("The array contains \(dataSet.count) elements.")
+
+for i in 0...dataSet.count - 1{
+    print("At index \(i) there is a value of \(dataSet[i])")
+}
 
